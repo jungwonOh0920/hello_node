20a54818671a27871659bd3e831fb73eea60d129
Jay Oh <jungwon.oh0920@gmail.com>
2023-08-08 14:46:00 -0700

    delete added

--- src/app.js
+++ src/app.js
@@ -45,7 +45,7 @@ app.get('/api/customers/:id', async (req, res) => {
         const customer = await Customer.findById(req.params.id)
         console.log(customer)
         if (!customer) {
-            res.status(404).json({error: 'user not found'})
+            res.status(404).json({error: '404 - user not found'})
         } else {
             res.json({customer})
         }
@@ -77,6 +77,16 @@ app.post('/api/customers', async (req, res) => {
         res.status(201).json({customer})
     } catch (e) {
         res.status(400).send({error: e.message})
+    }
+})
+
+app.delete('/api/customers/:id', async (req, res) => {
+    try {
+        const customerId = req.params.id
+        const result = await Customer.deleteOne({_id: customerId})
+        res.json({deletedCount: result.deletedCount})
+    } catch (e) {
+        res.status(500).json({error: 'something went wrong. cannot delete it. '})
     }
 })
 
